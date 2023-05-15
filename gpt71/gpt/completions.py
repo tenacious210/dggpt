28e94bb3a2461c95eba5a4680d4b035c190c6f50
tenacious210 <tenacious210@gmail.com>
2023-05-15 11:51:02 +0000

    added a generic rate limit error

--- gpt71/gpt/completions.py
+++ gpt71/gpt/completions.py
@@ -1,6 +1,7 @@
 # Handles all requests to openai
 import logging
 import openai
+from openai.error import RateLimitError
 from gpt71.config import OPENAI_KEY, add_monthly_tokens
 
 logger = logging.getLogger(__name__)
@@ -36,12 +37,19 @@ def chat_completion(convo: list[dict]) -> list[dict]:
     Gets a chat completion from openai.
     Takes in an openai convo, returns the updated convo.
     """
-    logger.info(f"Sending chat request\n  Input: {convo[-1]}")
-    rsp = openai.ChatCompletion.create(
-        model=CHAT_MODEL,
-        max_tokens=MAX_TOKENS,
-        messages=convo,
-    )
+
+    logger.info(f"Sending chat request...\n  Input: {convo[-1]}")
+    try:
+        rsp = openai.ChatCompletion.create(
+            model=CHAT_MODEL,
+            max_tokens=MAX_TOKENS,
+            messages=convo,
+        )
+    except RateLimitError:
+        rate_limit_message = "sorry, I got rate limited MMMM try again later"
+        logger.info("Got rate limited.")
+        convo.append({"role": "assistant", "content": rate_limit_message})
+        return convo
     message = dict(rsp["choices"][0]["message"])
     convo.append(message)
     logger.info(f"Chat completion recieved\n  Output: {message}")
