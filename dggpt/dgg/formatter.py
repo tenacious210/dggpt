f2ac261c459668fba4c8edbd520bf6aebfcf72ab
tenacious210 <tenacious210@gmail.com>
2023-07-26 18:38:07 +0000

    hard coded usernames

--- dggpt/dgg/formatter.py
+++ dggpt/dgg/formatter.py
@@ -13,6 +13,12 @@ def format_dgg_message(message: str, nick: str = None) -> str:
             message = message.replace(f"{emote}{punc}", f"{emote} {punc}")
             message = message.replace(f"{punc}{emote}", f"{punc} {emote}")
     message = message.replace("\n", " ")
+    if (
+        nick
+        and nick not in message
+        and not any((message.startswith(c) for c in (">", "!", "/me", "ඞ")))
+    ):
+        message = f"{nick} {message}"
     if len(message) > 512:
         message = message[:511]
     return message
