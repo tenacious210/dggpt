71e881b8a08e7cf7752b7696923f77636b92838f
tenacious210 <tenacious210@gmail.com>
2025-01-01 15:29:15 -0500

    no longer plays the mp3 file

--- dggpt/tts/tts.py
+++ dggpt/tts/tts.py
@@ -3,7 +3,6 @@ import logging
 
 from elevenlabs.client import ElevenLabs
 from elevenlabs import save
-from pygame import mixer
 
 from dggpt.config import ELEVENLABS_KEY
 
@@ -11,16 +10,11 @@ logger = logging.getLogger(__name__)
 client = ElevenLabs(api_key=ELEVENLABS_KEY)
 
 VOICE = "Jessica"
-MODEL = "eleven_multilingual_v2"
-# OUTPUT_DEVICE = "VB-Cable"
-OUTPUT_DEVICE = "Speakers (VB-Audio Virtual Cable)"
-# OUTPUT_DEVICE = "Speakers (Realtek(R) Audio)"
-
-mixer.init(devicename=OUTPUT_DEVICE, buffer=4096)
+MODEL = "eleven_flash_v2_5"
 
 
-def generate_tts(input: str = "Hello World"):
-    """Takes in a string and outputs a TTS to the selected output device"""
+def generate_tts(input: str):
+    """Takes in a string and outputs a TTS"""
     audio = client.generate(
         text=input,
         voice=VOICE,
@@ -28,7 +22,3 @@ def generate_tts(input: str = "Hello World"):
     )
     filename = "mp3files/" + datetime.now().strftime("%Y%m%d%H%M%S") + ".mp3"
     save(audio, filename)
-    # filename = "mp3files/test.mp3"
-    mixer.music.load(filename)
-    logger.info(f"Playing audio for {input}")
-    mixer.music.play()
